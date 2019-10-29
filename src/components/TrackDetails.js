30b0384655393b9921b16fdf717fbfa254e3447a
james-roberts-web <jamesroberts@xponorth.co.uk>
2019-10-29 21:47:27 +0000

    css update

--- src/components/TrackDetails.js
+++ src/components/TrackDetails.js
@@ -5,19 +5,18 @@ const TrackDetails = ({track}) => {
   return (
 
     <div className="track-details">
-      <div className="track-and-artist">
-        <div className="track-name">
-          <h3>{track["im:name"].label}</h3>
-        </div>
-        <div className="artist-name">
-          <h4>{track["im:artist"].label}</h4>
-        </div>
-      </div>
       <div className="release-image">
         <img src={track["im:image"][1].label}/>
       </div>
+      <div className="track-and-artist">
+        <div className="artist-name">
+          <h4>{track["im:artist"].label}</h4>
+        </div>
+        <div className="track-name">
+          <h3>{track["im:name"].label}</h3>
+        </div>
+      </div>
     </div>
-
   )
 }
 
